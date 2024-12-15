44d8a2ba72ca3430757d8101f005127fb39e4340
chamikara94 <viduranga1234@gmail.com>
2024-12-15 11:07:18 +0530

    Hello world added to the main method.

--- src/main/java/com/Student_info/StudentInfoApplication.java
+++ src/main/java/com/Student_info/StudentInfoApplication.java
@@ -8,7 +8,7 @@ public class StudentInfoApplication {
 
 	public static void main(String[] args) {
 
-
+		System.out.println("Hello World");
 		SpringApplication.run(StudentInfoApplication.class, args);
 	}
 
